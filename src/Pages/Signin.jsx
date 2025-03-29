b287fc9320b68f7da15285ab83394e348312494b
cHANGTEEZY <jagatgurung30@gmail.com>
2025-03-30 04:08:06 +0545

    added loading states

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -10,6 +10,7 @@ const Signin = () => {
   });
 
   const [errors, setErrors] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFormData = (e) => {
     setFormData({
@@ -18,69 +19,92 @@ const Signin = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     const formErrors = validateFormData(formData);
-
-    console.log("Validation Errors:", formErrors);
-
     if (Object.keys(formErrors).length > 0) {
       setErrors(formErrors);
       return;
     }
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `http://localhost:3000/users?email=${formData.email}&password=${formData.password}`
+      );
 
-    console.log("Signed in successfully!", formData);
+      const users = await response.json();
 
-    setFormData({
-      email: "",
-      password: "",
-    });
+      if (users.length === 0) {
+        setErrors({
+          email: "Wrong email or password",
+          password: "Wrong email or password",
+        });
+        return;
+      }
 
-    setErrors({});
+      localStorage.setItem("userAuthToken", users[0].id);
+
+      setFormData({
+        email: "",
+        password: "",
+      });
+
+      setErrors({});
+    } catch (error) {
+      console.error("Error during sign-in:", error);
+      setErrors({ email: "Something went wrong. Please try again later." });
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <>
-    <Header/>
-    <section className="auth-section">
-      <div className="Signin-container">
-        <div className="form-image">
-          <div className="brand-logo">Amnil Technologies</div>
-          <div className="image-caption">
-            <h2>Capturing Moments,</h2>
-            <p>Creating Memories</p>
+      <Header />
+      <section className="auth-section">
+        <div className="Signin-container">
+          <div className="form-image">
+            <div className="brand-logo">Amnil Technologies</div>
+            <div className="image-caption">
+              <h2>Capturing Moments,</h2>
+              <p>Creating Memories</p>
+            </div>
+          </div>
+          <div className="Signin-box">
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+              <Input
+                type="email"
+                placeholder="example@email.com"
+                name="email"
+                label="Email"
+                value={formData.email}
+                onChange={handleFormData}
+                error={errors.email}
+              />
+
+              <Input
+                type="password"
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+                value={formData.password}
+                onChange={handleFormData}
+                error={errors.password}
+              />
+
+              <button
+                type="submit"
+                className="submit-button"
+                disabled={isLoading}
+              >
+                {isLoading ? "Logging in...." : "Sign In"}
+              </button>
+            </form>
           </div>
         </div>
-        <div className="Signin-box">
-          <h1>Sign In</h1>
-          <form onSubmit={handleSubmit}>
-            <Input
-              type="email"
-              placeholder="example@email.com"
-              name="email"
-              label="Email"
-              value={formData.email}
-              onChange={handleFormData}
-              error={errors.email}
-            />
-
-            <Input
-              type="password"
-              name="password"
-              label="Password"
-              placeholder="Enter your password"
-              value={formData.password}
-              onChange={handleFormData}
-              error={errors.password}
-            />
-
-            <button type="submit" className="submit-button">
-              Sign In
-            </button>
-          </form>
-        </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -12,7 +12,8 @@ const Signup = () => {
     confirmPassword: "",
   });
 
-  const [errors, setErrors] = useState({}); 
+  const [errors, setErrors] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFormData = (e) => {
     setFormData({
@@ -22,122 +23,129 @@ const Signup = () => {
   };
 
   const handleSubmit = async (e) => {
-  e.preventDefault();
-  const formErrors = validateFormData(formData, true);
+    e.preventDefault();
+    const formErrors = validateFormData(formData, true);
 
-  if (Object.keys(formErrors).length > 0) {
-    setErrors(formErrors);
-    return;
-  }
-
-  try {
-    const existingUserResponse = await fetch(
-      `http://localhost:3000/users?email=${formData.email}`
-    );
-    const existingUsers = await existingUserResponse.json();
-
-    if (existingUsers.length > 0) {
-      setErrors({ email: "Email already exists. Please use another email." });
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
       return;
     }
 
-    const response = await fetch("http://localhost:3000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
+    setIsLoading(true);
 
-    if (!response.ok) {
-      throw new Error("Error Creating Account");
+    try {
+      const existingUserResponse = await fetch(
+        `http://localhost:3000/users?email=${formData.email}`
+      );
+      const existingUsers = await existingUserResponse.json();
+
+      if (existingUsers.length > 0) {
+        setErrors({ email: "Email already exists. Please use another email." });
+        return;
+      }
+
+      const response = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error Creating Account");
+      }
+
+      console.log("Success");
+
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+
+      setErrors({});
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setIsLoading(false);
     }
-
-    console.log("Success");
-
-    setFormData({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-    });
-
-    setErrors({});
-  } catch (error) {
-    console.error(error.message);
-  }
-};
-
+  };
 
   return (
     <>
-    <Header/>
-    <section className="auth-section">
-      <div className="signup-container">
-        <div className="form-image">
-          <div className="brand-logo">Amnil Technologies</div>
-          <div className="image-caption">
-            <h2>Capturing Moments,</h2>
-            <p>Creating Memories</p>
+      <Header />
+      <section className="auth-section">
+        <div className="signup-container">
+          <div className="form-image">
+            <div className="brand-logo">Amnil Technologies</div>
+            <div className="image-caption">
+              <h2>Capturing Moments,</h2>
+              <p>Creating Memories</p>
+            </div>
+          </div>
+          <div className="signup-box">
+            <h1>Create an account</h1>
+            <form onSubmit={handleSubmit}>
+              <Input
+                type="text"
+                placeholder="Sushank"
+                name="firstName"
+                label="First Name"
+                value={formData.firstName}
+                onChange={handleFormData}
+                error={errors.firstName}
+              />
+              <Input
+                type="text"
+                placeholder="Gurung"
+                name="lastName"
+                label="Last Name"
+                value={formData.lastName}
+                onChange={handleFormData}
+                error={errors.lastName}
+              />
+              <Input
+                type="email"
+                placeholder="example@email.com"
+                name="email"
+                label="Email"
+                value={formData.email}
+                onChange={handleFormData}
+                error={errors.email}
+              />
+              <Input
+                type="password"
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+                value={formData.password}
+                onChange={handleFormData}
+                error={errors.password}
+              />
+
+              <Input
+                type="password"
+                name="confirmPassword"
+                label="Confirm Password"
+                placeholder="Re-enter your password"
+                value={formData.confirmPassword}
+                onChange={handleFormData}
+                error={errors.confirmPassword}
+              />
+              <button
+                type="submit"
+                className="submit-button"
+                disabled={isLoading}
+              >
+                {isLoading ? "Creating..." : "Create account"}
+              </button>
+            </form>
           </div>
         </div>
-        <div className="signup-box">
-          <h1>Create an account</h1>
-          <form onSubmit={handleSubmit}>
-            <Input
-              type="text"
-              placeholder="Sushank"
-              name="firstName"
-              label="First Name"
-              value={formData.firstName}
-              onChange={handleFormData}
-              error={errors.firstName}
-            />
-            <Input
-              type="text"
-              placeholder="Gurung"
-              name="lastName"
-              label="Last Name"
-              value={formData.lastName}
-              onChange={handleFormData}
-              error={errors.lastName}
-            />
-            <Input
-              type="email"
-              placeholder="example@email.com"
-              name="email"
-              label="Email"
-              value={formData.email}
-              onChange={handleFormData}
-              error={errors.email}
-            />
-            <Input
-              type="password"
-              name="password"
-              label="Password"
-              placeholder="Enter your password"
-              value={formData.password}
-              onChange={handleFormData}
-              error={errors.password}
-            />
-
-            <Input
-              type="password"
-              name="confirmPassword"
-              label="Confirm Password"
-              placeholder="Re-enter your password"
-              value={formData.confirmPassword}
-              onChange={handleFormData}
-              error={errors.confirmPassword}
-            />
-            <button type="submit" className="submit-button">
-              Create account
-            </button>
-          </form>
-        </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
